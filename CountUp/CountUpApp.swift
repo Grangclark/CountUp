5c0c5f6c728f3cf48f6344f01ff83109bb25d07c
Grangclark <96944049+Grangclark@users.noreply.github.com>
2024-12-07 22:43:46 +0900

    Initial Commit

---
+++ CountUp/CountUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountUpApp.swift
+//  CountUp
+//
+//  Created by 長橋和敏 on 2024/12/07.
+//
+
+import SwiftUI
+
+@main
+struct CountUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
